85720c030dd53480c2d9bf9d42733e80e33401e2
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-29 20:43:48 +0600

    prodoct created in database, and connect database

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// mongo
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mrtools.5n8hr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const productCollection = client.db("mrTools").collection("product");
+
+        // get all products
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+            console.log('products data send')
+        });
+
+    }
+    finally {
+        // client.close();
+    }
+};
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
