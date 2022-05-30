57cdbe19e644e996325c837e57752e78a8f3dff9
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-30 20:53:03 +0600

    single product fix

--- index.js
+++ index.js
@@ -35,8 +35,9 @@ const run = async () => {
         // get single product
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
+            console.log(id)
             const query = { _id: ObjectId(id) };
-            const product = await productCollection.find(query);
+            const product = await productCollection.findOne(query);
             res.send(product);
             console.log('single product data send')
         });
